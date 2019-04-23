87c932c39d8625148a046dcef96d3af49bbe5bc2
aribahtiarpro <aribahtiarpro@gmail.com>
2019-04-23 14:38:20 +0700

    update service worker

---
+++ sw.js
@@ -0,0 +1,56 @@
+const version = "1";
+const cacheName = `u-nesia-${version}`;
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(cacheName).then(cache => {
+      return cache.addAll([
+          `/`,
+          `/fonts/themify.eot`,
+          `/fonts/themify.svg`,
+          `/fonts/themify.ttf`,
+          `/fonts/themify.woff`,
+          `/css/bootstrap.min.css`,
+          `/css/owl.carousel.min.css`,
+          `/css/style.css`,
+          `/css/themify-icons.css`,
+          `/images/appleicon.png`,
+          `/images/client.png`,
+          `/images/dualphone.png`,
+          `/images/favicon.png`,
+          `/images/graphic.png`,
+          `/images/iphonex.png`,
+          `/images/logo.png`,
+          `/images/logoa.png`,
+          `/images/perspective.png`,
+          `/images/playicon.png`,
+          `/images/screen1.jpg`,
+          `/images/screen2.jpg`,
+          `/images/screen3.jpg`,
+          `/js/bootstrap.bundle.min.js`,
+          `/js/jquery-3.2.1.min.js`,
+          `/js/owl.carousel.min.js`,
+          `/js/script.js`,
+          `index.html`
+        ])
+        .then(() => self.skipWaiting());
+    })
+  );
+});
+
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.open(cacheName)
+    .then(cache => cache.match(event.request, {
+      ignoreSearch: true
+    }))
+    .then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
