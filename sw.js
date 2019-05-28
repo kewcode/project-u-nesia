19fec206b2c49d921085c5afb745a92d860a9294
aribahtiarpro <aribahtiarpro@gmail.com>
2019-05-28 17:10:07 +0700

    upgrate

--- sw.js
+++ sw.js
@@ -5,29 +5,6 @@ self.addEventListener('install', e => {
     caches.open(cacheName).then(cache => {
       return cache.addAll([
           `/`
-          // `/fonts/themify.eot`,
-          // `/fonts/themify.svg`,
-          // `/fonts/themify.ttf`,
-          // `/fonts/themify.woff`,
-          `/css/bootstrap.min.css`,
-          `/css/owl.carousel.min.css`,
-          // `/css/style.css`,
-          `/css/themify-icons.css`,
-          `/images/appleicon.png`,
-          `/images/client.png`,
-          `/images/dualphone.png`,
-          `/images/favicon.png`,
-          `/images/graphic.png`,
-          `/images/iphonex.png`,
-          `/images/logo.png`,
-          `/images/perspective.png`,
-          `/images/playicon.png`,
-          `/images/screen1.jpg`,
-          `/images/screen2.jpg`,
-          `/images/screen3.jpg`,
-          `/js/bootstrap.bundle.min.js`,
-          `/js/jquery-3.2.1.min.js`,
-          `/js/owl.carousel.min.js`
         ])
         .then(() => self.skipWaiting());
     })
